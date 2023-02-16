8b07f9c11ab874e4c94cb5f2668c363fb7c21583
DaniGM17 <daniagm75@gmail.com>
2023-02-16 11:58:31 -0600

    Llamado de funciones como parametro

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -9,6 +9,22 @@ class CounterScreen extends StatefulWidget {
 
 class _CounterScreenState extends State<CounterScreen> {
   int contador = 0;
+
+  void incrementar() {
+    contador++;
+    setState(() {});
+  }
+
+  void decrementar() {
+    contador--;
+    setState(() {});
+  }
+
+  void reinciar() {
+    contador=0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     const TextStyle estiloTexto = TextStyle(
@@ -38,26 +54,44 @@ class _CounterScreenState extends State<CounterScreen> {
         ],
       )),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          FloatingActionButton(
-            backgroundColor: Colors.pink,
-            onPressed: () => setState(() => contador--),
-            child: const Icon(Icons.exposure_minus_1),
-          ),
-          FloatingActionButton(
-            backgroundColor: Colors.pink,
-            onPressed: () => setState(() => contador=0),
-            child: const Icon(Icons.restart_alt),
-          ),
-          FloatingActionButton(
-            backgroundColor: Colors.pink,
-            onPressed: () =>  setState(() =>contador++),
-            child: const Icon(Icons.exposure_plus_1),
-          ),
-        ],
-      ),
+      floatingActionButton: CustomFloatingActionButtom(
+        decrease: decrementar, 
+        increase: incrementar, 
+        reset: reinciar,
+        ));
+  }
+}
+
+class CustomFloatingActionButtom extends StatelessWidget {
+  final Function increase;
+  final Function decrease;
+  final Function reset;
+
+  const CustomFloatingActionButtom({
+    super.key, required this.increase, required this.decrease, required this.reset,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          backgroundColor: Colors.pink,
+          onPressed: () => decrease,
+          child: const Icon(Icons.exposure_minus_1),
+        ),
+        FloatingActionButton(
+          backgroundColor: Colors.pink,
+          onPressed: () => reset,
+          child: const Icon(Icons.restart_alt),
+        ),
+        FloatingActionButton(
+          backgroundColor: Colors.pink,
+          onPressed: () => increase,
+          child: const Icon(Icons.exposure_plus_1),
+        ),
+      ],
     );
   }
 }
